628670a2c675cbcf0d8cb1c7a4a05bf064828f35
Donny Marthen Sitompul <79772754+dms-codes@users.noreply.github.com>
2022-09-22 16:23:16 +0700

    Create guess_secret_number.py

---
+++ guess_secret_number.py
@@ -0,0 +1,16 @@
+secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+while int(input())!=secret_number:
+    print("Ha ha! You're stuck in my loop!")
+print("Well done, muggle! You are free now.")
