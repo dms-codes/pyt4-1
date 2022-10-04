3fb557f0b873d4ff928d4b3525a3a3b4d78da1b6
Donny Marthen Sitompul <donnymarthen.sitompul@gmail.com>
2022-10-04 19:52:37 +0700

    Create led_display.py

---
+++ led_display.py
@@ -0,0 +1,40 @@
+def get_led():
+    led_string = """
+  # ### ### # # ### ### ### ### ### ### 
+  #   #   # # # #   #     # # # # # # # 
+  # ### ### ### ### ###   # ### ### # # 
+  # #     #   #   # # #   # # #   # # # 
+  # ### ###   # ### ###   # ### ### ###"""
+      
+    lines = led_string.split("\n")[1:]
+    for line in lines:
+        print(line)
+    led = {
+        '1':[],
+        '2':[],
+        '3':[],
+        '4':[],
+        '5':[],
+        '6':[],
+        '7':[],
+        '8':[],
+        '9':[],
+        '0':[],
+    }
+
+    for line in lines:
+        for i,v in enumerate(led.keys()):
+            led[v].append(line[4*i:4*i+3])
+    return led
+    
+def turnon(num,led):
+    num = list(num)
+    for line in range(5):
+        line_string = ""
+        for n in num:
+            line_string += led[n][line]+" "
+        print(line_string)
+        
+if __name__ == "__main__":
+    led = get_led()
+    turnon("123",led)
